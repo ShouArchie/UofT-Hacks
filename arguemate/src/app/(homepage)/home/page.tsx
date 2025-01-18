991600e8cbab681177c80717cfb7868f93c18e98
karen guan <93009257+karenguann@users.noreply.github.com>
2025-01-18 00:45:56 -0500

    bg

--- arguemate/src/app/(homepage)/home/page.tsx
+++ arguemate/src/app/(homepage)/home/page.tsx
@@ -7,7 +7,12 @@ const HomePage: React.FC = () => {
       <nav className="navbar">
         <ul>
           <li>
-            <a href="/about">About</a>
+            <a
+              href="/about"
+              className="text-foreground bg-background hover:bg-[#FFD0A0] focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900"
+            >
+              About
+            </a>
           </li>
           <li>
             <a
